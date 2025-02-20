236b45f25a32fc82e94c1f1b61f53d4f3921f2a4
Lai <62993209+CHIHCHIEH-LAI@users.noreply.github.com>
2025-02-20 18:09:02 +0800

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec docker run --privileged -p 6666:6666 \
+exec docker run --privileged -p 8082:8082 \
     -e S3_BUCKET="package" \
     -e S3_REGION="ap-northeast-1" \
     pypiserver:0.1.0
